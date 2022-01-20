9539abff5125ae3e5097ed028292b14bb1f4c7d0
Mailari <mailarighulihond@gmail.com>
2022-01-20 10:18:22 +0530

    intial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Nav from "./components/nav";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./components/about";
+import Work from "./components/work";
+import Resume from "./components/resume";
+import { useEffect, useState } from "react";
 function App() {
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") === "true" ? true : false
+  );
+
+  const updateColors = () => {
+    if (theme) {
+      document.body.style.backgroundColor = "white";
+      document.body.style.color = "black";
+    } else {
+      document.body.style.backgroundColor = "rgb(46, 45, 45)";
+      document.body.style.color = "white";
+    }
+  };
+
+  useEffect(() => updateColors());
+
+  const onThemeChange = () => {
+    localStorage.setItem("theme", !theme);
+    setTheme(!theme);
+    updateColors();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Nav changeTheme={onThemeChange} />
+      {theme}
+      <Routes>
+        <Route path="/" element={Work()}></Route>
+        <Route path="/about" element={About()}></Route>
+        <Route path="/resume" element={Resume()}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
