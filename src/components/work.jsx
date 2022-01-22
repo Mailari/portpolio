035182e7f77e152c9038baf128c11daa7f4de10b
Mailari <mailarighulihond@gmail.com>
2022-01-22 23:33:34 +0530

    minor change

--- src/components/work.jsx
+++ src/components/work.jsx
@@ -1,36 +1,34 @@
 import React from "react";
-
+import gpstek from "./../assets/gpstek.png";
+import sa from "./../assets/sa.png";
 export default function Work() {
   return (
-    <div className="mt-[10vh] mx-[20%] flex flex-row">
-      <div className="w-[35%] border-8">
-        <h2>p1</h2>
-        <h2>p2</h2>
-        <h2>p3</h2>
-        <h2>p4</h2>
-        <h2>p5</h2>
-        <h2>p6</h2>
+    <div className="">
+      <div className="flex flex-col-reverse justify-center md:flex-row text-center">
+        <div className="m-10">
+          <h2>GPSTEK.in</h2>
+          <button className="my-2 rounded-lg border-2">
+            <a href="https://gpstek.in">
+              <p className="px-5">View Site</p>
+            </a>
+          </button>
+        </div>
+        <img
+          className="w-[80%] m-4 md:m-10  md:w-[40%] self-center"
+          src={gpstek}
+        />
       </div>
-
-      <div className="flex flex-col">
-        <h1 id="1" className="my-[10vh]">
-          Project 1
-        </h1>
-        <h1 id="2" className="my-[10vh]">
-          Project 2
-        </h1>
-        <h1 id="3" className="my-[10vh]">
-          Project 3
-        </h1>
-        <h1 id="4" className="my-[10vh]">
-          Project 4
-        </h1>
-        <h1 id="5" className="my-[10vh]">
-          Project 5
-        </h1>
-        <h1 id="6" className="my-[10vh]">
-          Project 6
-        </h1>
+      <br />
+      <div className="flex flex-col justify-center md:flex-row text-center">
+        <img className="w-[80%] m-4 md:m-10  md:w-[40%] self-center" src={sa} />
+        <div className="m-10">
+          <h2>sacapital.co.in</h2>
+          <button className="my-2 rounded-lg border-2">
+            <a href="https://sacapital.co.in">
+              <p className="px-5">View Site</p>
+            </a>
+          </button>
+        </div>
       </div>
     </div>
   );
